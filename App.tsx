c98e16deb6f0ae2626c4fb78b018288c090113ae
chaitanya <chaitanya.rongali@everest.engineering>
2025-10-24 16:15:44 +0530

    Add safeprovider to the app component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Displaylistofitems from './src/screens/itemlist';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-      
+      <SafeAreaProvider>
       <Displaylistofitems/>
+      </SafeAreaProvider>
       
     
   );
